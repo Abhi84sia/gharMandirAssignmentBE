afd9972ae745443e1728aabb0b22099f0c897d3c
Abhishekcoutloot <abhishekc@coutloot.com>
2025-03-31 11:57:57 +0530

    some fixes for deployment

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors'); // Import the cors module
-const panchangRoutes = require('./routes/panchangRoutes');
+const cors = require('cors');
 const fs = require('fs');
+const http = require('http');
 const https = require('https');
-
-
+const panchangRoutes = require('./routes/panchangRoutes');
 
 dotenv.config();
+
+const app = express();
+const PORT_HTTP = process.env.PORT_HTTP || 5001;
+const PORT_HTTPS = process.env.PORT_HTTPS || 5000;
+
 const options = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem'),
 };
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-
-
-app.use(cors({
-  origin: 'https://ghar-mandir-assignment-fe.vercel.app',
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type'],
-}));
+app.use(cors());
 app.use(express.json());
 
-// Mount routes
+app.use((req, res, next) => {
+  if (!req.secure) {
+    return res.redirect(`https://${req.headers.host.replace(PORT_HTTP, PORT_HTTPS)}${req.url}`);
+  }
+  next();
+});
+
+// Routes
 app.use('/api', panchangRoutes);
 
-https.createServer(options, app).listen(PORT, () => {
-  console.log('Server running on https://35.154.137.159:3003');
+http.createServer(app).listen(PORT_HTTP, () => {
+  console.log(`HTTP Server running on http://35.154.137.159:${PORT_HTTP}`);
+});
+
+https.createServer(options, app).listen(PORT_HTTPS, () => {
+  console.log(`HTTPS Server running on https://35.154.137.159:${PORT_HTTPS}`);
 });
