fa07290944ba7b15af81a10f472a1ccc08e7ae00
Abhishekcoutloot <abhishekc@coutloot.com>
2025-03-31 12:06:13 +0530

    fixes

--- server.js
+++ server.js
@@ -1,39 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors');
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
+const cors = require('cors'); 
 const panchangRoutes = require('./routes/panchangRoutes');
 
 dotenv.config();
 
 const app = express();
-const PORT_HTTP = process.env.PORT_HTTP || 5001;
-const PORT_HTTPS = process.env.PORT_HTTPS || 5000;
+const PORT = process.env.PORT || 5000;
 
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
-};
+app.use(cors()); 
 
-app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  if (!req.secure) {
-    return res.redirect(`https://${req.headers.host.replace(PORT_HTTP, PORT_HTTPS)}${req.url}`);
-  }
-  next();
-});
-
-// Routes
+// Mount routes
 app.use('/api', panchangRoutes);
 
-http.createServer(app).listen(PORT_HTTP, () => {
-  console.log(`HTTP Server running on http://35.154.137.159:${PORT_HTTP}`);
-});
-
-https.createServer(options, app).listen(PORT_HTTPS, () => {
-  console.log(`HTTPS Server running on https://35.154.137.159:${PORT_HTTPS}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
