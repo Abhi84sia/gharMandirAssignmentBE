02c2749c729e8331cc7b372341e2fb6740026390
Abhishekcoutloot <abhishekc@coutloot.com>
2025-03-31 09:21:52 +0530

    changed server

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors'); // Import the cors module
 const panchangRoutes = require('./routes/panchangRoutes');
+const fs = require('fs');
+const https = require('https');
+
+
 
 dotenv.config();
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+};
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
 
 app.use(cors({
   origin: 'https://ghar-mandir-assignment-fe.vercel.app',
@@ -18,6 +28,6 @@ app.use(express.json());
 // Mount routes
 app.use('/api', panchangRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+https.createServer(options, app).listen(PORT, () => {
+  console.log('Server running on https://35.154.137.159:3003');
 });
